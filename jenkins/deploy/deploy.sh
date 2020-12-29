76d65ce9444c073af481602fdcce3099375c9362
EC2 Default User <ec2-user@ip-172-31-24-107.ec2.internal>
2020-12-29 22:56:25 +0000

    updated ssh key location

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /home/ec2-user/prod /tmp/.auth prod-user@172.31.50.200:/tmp/.auth
-scp -i /home/ec2-user/prod ./jenkins/deploy/publish prod-user@172.31.50.200:/tmp/publish
-ssh -i /home/ec2-user/prod prod-user@172.31.50.200 "/tmp/publish"
+scp -i /var/jenkins_home/ssh/prod /tmp/.auth prod-user@172.31.50.200:/tmp/.auth
+scp -i /var/jenkins_home/ssh/prod ./jenkins/deploy/publish prod-user@172.31.50.200:/tmp/publish
+ssh -i /var/jenkins_home/ssh/prod prod-user@172.31.50.200 "/tmp/publish"
