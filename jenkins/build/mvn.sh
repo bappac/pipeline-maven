35dbea0d3621bf5891eaf584ec21cde162459792
EC2 Default User <ec2-user@ip-172-31-24-107.ec2.internal>
2020-12-30 01:35:40 +0000

    correcting build path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,6 +6,6 @@ echo "***************************"
 
 #WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/home/ec2-user/jenkins/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
