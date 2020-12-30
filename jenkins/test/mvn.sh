aff09137bd0e11e5b25ae3e105c16b2d7ef67baa
EC2 Default User <ec2-user@ip-172-31-24-107.ec2.internal>
2020-12-30 02:45:32 +0000

    updated test

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,8 @@ echo "***************************"
 echo "** Testing the code *******"
 echo "***************************"
 
-WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+#WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+WORKSPACE=/home/ec2-user/jenkins/jenkins_home/workspace/pipeline-docker-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
